e8796aa7676961c8b3683e6b672f6681e987cea8
Marios <geom4rios@gmail.com>
2017-12-16 21:40:11 +0200

    Adding transactions with the db

--- src/com/simpleprogrammer/Program.java
+++ src/com/simpleprogrammer/Program.java
@@ -1,13 +1,34 @@
 package com.simpleprogrammer;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 public class Program {
     public static void main(String[] args) {
         System.out.println("hello world");
-
         Session session = HibernateUtilities.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        User user = new User();
+        user.setName("Joe");
+        user.setGoal(250);
+        session.save(user);
+
+        session.getTransaction().commit();
+
+
+        session.beginTransaction();
+        User loadedUser = (User) session.get(User.class, 1);
+        System.out.println(loadedUser.getName());
+        System.out.println(loadedUser.getGoal());
+
+        loadedUser.setTotal(loadedUser.getTotal() + 50);
+
+
+        session.getTransaction().commit();
+
         session.close();
+        HibernateUtilities.getSessionFactory().close();
 
     }
 }
